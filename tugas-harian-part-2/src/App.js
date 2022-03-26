28fe40a72d47a0163d15fa3120dab0576f175b1c
NunoAlwi <nunoalwi@gmail.com>
2022-03-26 07:49:34 +0700

    tugas:hari-1-part2

---
+++ tugas-harian-part-2/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import Logo from '../src/Assets/image/logo.png';
+
+const ListComponent = (props) => {
+	return (
+		<div class="input-checkbox">
+			<input type="checkbox" />
+			<p>{props.listData}</p>
+		</div>
+	);
+};
+
+function App() {
+	return (
+		<div className="card">
+			<img src={Logo} />
+			<p>THINGS TO DO</p>
+			<small>During Bootcamp in Sanbercode</small>
+			<hr />
+
+			<ListComponent listData="Belajar Git & CLI" />
+			<ListComponent listData="Belajar HTML & CSS" />
+			<ListComponent listData="Belajar Javascript" />
+			<ListComponent listData="Belajar ReactJS Dasar" />
+			<ListComponent listData="Belajar ReactJS Advance" />
+
+			<button>Send</button>
+		</div>
+	);
+}
+
+export default App;
